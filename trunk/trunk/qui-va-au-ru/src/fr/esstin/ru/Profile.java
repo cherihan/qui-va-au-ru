9e0b44b6a8ceb97b6685bae0f80867bf97e4d38d
philippe.lepori@gmail.com <philippe.lepori@gmail.com@8f962abb-a62f-6ecb-c074-8948adae5ade>
2012-01-10 14:56:18 +0000

    Update Activities

--- trunk/trunk/qui-va-au-ru/src/fr/esstin/ru/Profile.java
+++ trunk/trunk/qui-va-au-ru/src/fr/esstin/ru/Profile.java
@@ -1,7 +1,15 @@
 package fr.esstin.ru;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class Profile extends PreferenceActivity {
-	
+	SharedPreferences pref;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.profile);
+		pref = getPreferenceManager().getSharedPreferences();
+	}
 }
