a3fd04e1760a331a324c418f2fefa834392fcc52
philippe.lepori@gmail.com <philippe.lepori@gmail.com@8f962abb-a62f-6ecb-c074-8948adae5ade>
2012-01-10 10:27:31 +0000

    Ajout des préférences

--- trunk/trunk/qui-va-au-ru/gen/fr/esstin/ru/R.java
+++ trunk/trunk/qui-va-au-ru/gen/fr/esstin/ru/R.java
@@ -14,20 +14,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int cb_carpooling=0x7f05000a;
-        public static final int cb_going=0x7f050009;
-        public static final int ll_infos=0x7f050005;
-        public static final int lv_carpoolers=0x7f05000c;
-        public static final int lv_going=0x7f05000d;
-        public static final int rl_main=0x7f050004;
-        public static final int tv_carpools=0x7f050008;
-        public static final int tv_lv_going=0x7f050003;
-        public static final int tv_lv_nickname=0x7f050000;
-        public static final int tv_lv_seats=0x7f050002;
-        public static final int tv_lv_times=0x7f050001;
-        public static final int tv_nickname=0x7f050006;
-        public static final int tv_seats=0x7f050007;
-        public static final int tv_summary=0x7f05000b;
+        public static final int cb_carpooling=0x7f06000a;
+        public static final int cb_going=0x7f060009;
+        public static final int ll_infos=0x7f060005;
+        public static final int lv_carpoolers=0x7f06000c;
+        public static final int lv_going=0x7f06000d;
+        public static final int rl_main=0x7f060004;
+        public static final int tv_carpools=0x7f060008;
+        public static final int tv_lv_going=0x7f060003;
+        public static final int tv_lv_nickname=0x7f060000;
+        public static final int tv_lv_seats=0x7f060002;
+        public static final int tv_lv_times=0x7f060001;
+        public static final int tv_nickname=0x7f060006;
+        public static final int tv_seats=0x7f060007;
+        public static final int tv_summary=0x7f06000b;
     }
     public static final class layout {
         public static final int carpoolers=0x7f030000;
@@ -35,11 +35,15 @@ public final class R {
         public static final int main=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int carpooling=0x7f040004;
-        public static final int empty=0x7f040002;
-        public static final int going=0x7f040003;
-        public static final int hello=0x7f040000;
-        public static final int summary=0x7f040005;
+        public static final int app_name=0x7f050001;
+        public static final int carpooling=0x7f050004;
+        public static final int empty=0x7f050002;
+        public static final int going=0x7f050003;
+        public static final int hello=0x7f050000;
+        public static final int preferences_title=0x7f050006;
+        public static final int summary=0x7f050005;
+    }
+    public static final class xml {
+        public static final int profile=0x7f040000;
     }
 }
---
+++ trunk/trunk/qui-va-au-ru/src/fr/esstin/ru/Profile.java
@@ -0,0 +1,7 @@
+package fr.esstin.ru;
+
+import android.preference.PreferenceActivity;
+
+public class Profile extends PreferenceActivity {
+	
+}
