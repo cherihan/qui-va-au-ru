334916f4b85dc585b125152754d4b1b55edda410
philippe.lepori@gmail.com <philippe.lepori@gmail.com@8f962abb-a62f-6ecb-c074-8948adae5ade>
2012-01-10 22:57:53 +0000

    Update de l'UI

--- trunk/qui-va-au-ru/src/fr/esstin/ru/Profile.java
+++ trunk/qui-va-au-ru/src/fr/esstin/ru/Profile.java
@@ -1,15 +1,30 @@
 package fr.esstin.ru;
 
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-public class Profile extends PreferenceActivity {
-	SharedPreferences pref;
+public class Profile extends PreferenceActivity implements
+		OnSharedPreferenceChangeListener {
+	private static final int ACTIVITY_NUMBER = 2;
+	private static SharedPreferences pref;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.profile);
 		pref = getPreferenceManager().getSharedPreferences();
+		pref.registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		pref = getPreferenceManager().getSharedPreferences();
+	}
+
+	public SharedPreferences getPreferences() {
+		return pref;
 	}
 }
